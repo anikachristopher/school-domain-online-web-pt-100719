0c7b03470e066e1cca8ee7e58140db47f666cc04
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-05 04:33:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+ 
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+end
+
+
+
+
+
+
+
+
+
